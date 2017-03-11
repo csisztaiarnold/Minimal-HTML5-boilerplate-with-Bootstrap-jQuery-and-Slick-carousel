685f70ac5ccdf305c794fd9d668db83363fb0017
Csisztai Arnold <csisztai.arnold@gmail.com>
2017-03-11 21:44:56 +0100

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('.carousel-inner').slick({
+		dots: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		autoplay: true
+	});
+		
+});
---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/slick.css">
+	<link rel="stylesheet" href="assets/css/main.css?v=<?php echo rand(0,9999999); ?>">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<!--[if lt IE 9]>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+	<div class="container">
+	
+		<header>
+		
+			<nav></nav>
+			
+		</header>
+		
+		<main>
+			
+			<section class="carousel">
+			
+				<div class="carousel-inner">
+					<div>
+						<p>Lorem ipsum dolor sit amet consectetur adispicim elit</p>
+					</div>
+					<div>
+						<p>Integer vitae libero ac risus egestas placerat.</p>
+					</div>
+					<div>
+						<p>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi</p>
+					</div>
+					<div>
+						<p>Phasellus ultrices nulla quis nibh. Quisque a lectus. </p>
+					</div>
+					<div>
+						<p>Pellentesque fermentum dolor. Aliquam quam lectus</p>
+					</div>
+				</div>
+				
+			</section><!-- // Carousel -->
+			
+		</main>
+		
+		<footer>
+		
+		</footer>
+	
+	</div><!-- // Container -->
+	
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+	<script src="assets/js/slick.min.js"></script>
+	
+</body>
+</html>
